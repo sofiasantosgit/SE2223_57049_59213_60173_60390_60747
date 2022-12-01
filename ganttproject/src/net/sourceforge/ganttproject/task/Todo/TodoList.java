36d1881b466f186f1f493438939ecc89c1cdb9be
Tomas Mondim <t.mondim@campus.fct.unl.pt>
2022-12-01 15:06:22 +0000

    Added Todo Classes

---
+++ ganttproject/src/net/sourceforge/ganttproject/task/Todo/TodoList.java
@@ -0,0 +1,16 @@
+package net.sourceforge.ganttproject.task.Todo;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoList {
+    private final List<Todo> list;
+    public TodoList(){
+        list = new ArrayList<>();
+    }
+    public void add(Todo t){
+        list.add(t);
+    }
+    public List<Todo> list(){
+        return list;
+    }
+}
